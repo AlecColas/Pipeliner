6edaaa15a25d4af31b2fb71e08566530f55585a3
Alexandre Colas <alexandr.colas@free.fr>
2023-06-16 23:08:01 +0200

    feat(Pipeliner): initial commit with RN + TS template

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        semi: ['error', 'never'],
+        // this is for sorting WITHIN an import
+        'sort-imports': [
+          'error',
+          {ignoreCase: true, ignoreDeclarationSort: true},
+        ],
+        // this is for sorting imports
+        'import/order': [
+          'error',
+          {
+            groups: [
+              ['external', 'builtin'],
+              'internal',
+              ['sibling', 'parent'],
+              'index',
+            ],
+            pathGroups: [
+              {
+                pattern: '@(react|react-native)',
+                group: 'external',
+                position: 'before',
+              },
+              {
+                pattern: '@src/**',
+                group: 'internal',
+              },
+            ],
+            pathGroupsExcludedImportTypes: ['internal', 'react'],
+            'newlines-between': 'always',
+            alphabetize: {
+              order: 'asc',
+              caseInsensitive: true,
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
