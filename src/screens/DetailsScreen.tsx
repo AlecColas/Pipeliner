6764c9011d190024308c70fe1171a86c1fcc7db8
Alexandre Colas <alexandr.colas@free.fr>
2023-06-27 14:31:10 +0200

    refactor(Screen): Details Screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -4,12 +4,13 @@ import {StyleSheet, Text, View} from 'react-native'
 const DetailsScreen = () => {
   return (
     <View style={styles.center}>
-      <Text>Details Screen</Text>
+      <Text>Details!</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  center: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  center: {flex: 1, justifyContent: 'center', alignItems: 'center'},
 })
+
 export default DetailsScreen
