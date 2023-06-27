5f4529b805b3309fe4cce98bb84b086ca67b93c4
Alexandre Colas <alexandr.colas@free.fr>
2023-06-27 14:32:05 +0200

    feat(App): modify App with 3 tab-based navigation

--- App.tsx
+++ App.tsx
@@ -1,25 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react'
 
-import React from 'react'
-// import {useColorScheme} from 'react-native'
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {NavigationContainer} from '@react-navigation/native'
 import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
-// import {Colors} from 'react-native/Libraries/NewAppScreen'
 import DetailsScreen from './src/screens/DetailsScreen'
 import HomeScreen from './src/screens/HomeScreen'
+import SettingsScreen from './src/screens/SettingsScreen'
 
 type HomeStackParamList = {
   Home: undefined // undefined because you aren't passing any params to the home screen
@@ -31,23 +22,84 @@ export type HomeScreenNavigationProp = NativeStackNavigationProp<
   'Home'
 >
 
-const Stack = createNativeStackNavigator<HomeStackParamList>()
+const HomeStack = createNativeStackNavigator<HomeStackParamList>()
 
-const App = () => {
-  // const isDarkMode = useColorScheme() === 'dark'
-
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // }
-
+const HomeStackScreen = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
   )
 }
 
-export default App
+type SettingsScreenNavigationProp = NativeStackNavigationProp<
+  HomeStackParamList,
+  'Home'
+>
+
+export type SettingsScreenProps = {
+  navigation: SettingsScreenNavigationProp
+}
+
+type SettingStackParamList = {
+  Settings: undefined // undefined because you aren't passing any params to the home screen
+  Details: undefined // undefined because you aren't passing any params to the home screen
+}
+const SettingsStack = createNativeStackNavigator<SettingStackParamList>()
+
+const SettingsStackScreen = () => {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  )
+}
+
+type BoardStackParamList = {
+  Settings: undefined // undefined because you aren't passing any params to the home screen
+  Details: undefined // undefined because you aren't passing any params to the home screen
+}
+
+const BoardStack = createNativeStackNavigator<BoardStackParamList>()
+
+const BoardStackScreen = () => {
+  return (
+    <BoardStack.Navigator>
+      <BoardStack.Screen name="Settings" component={SettingsScreen} />
+      <BoardStack.Screen name="Details" component={DetailsScreen} />
+    </BoardStack.Navigator>
+  )
+}
+
+const Tab = createBottomTabNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = 'ios-calendar'
+
+            if (route.name === 'HomeScreen') {
+              iconName = focused ? 'ios-calendar' : 'ios-calendar-outline'
+            } else if (route.name === 'SettingsScreen') {
+              iconName = focused ? 'ios-settings' : 'ios-settings-outline'
+            } else if (route.name === 'BoardScreen') {
+              iconName = focused ? 'ios-grid' : 'ios-grid-outline'
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="BoardScreen" component={BoardStackScreen} />
+        <Tab.Screen name="HomeScreen" component={HomeStackScreen} />
+        <Tab.Screen name="SettingsScreen" component={SettingsStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
